5cebd90288b11e0b96f01cc5d3ef703d1ab8e690
ICTConsultant <jugroopsinghjugpal@gmail.com>
2025-08-12 20:36:58 +0100

    api weather https

--- resources/weather.js
+++ resources/weather.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const url = `http://api.weatherapi.com/v1/current.json?key=6a4e09f9f46e4a75a67171320251208&q=B76&aqi=no`;
+    const url = `https://api.weatherapi.com/v1/current.json?key=6a4e09f9f46e4a75a67171320251208&q=B76&aqi=no`;
     const weatherDisplay = document.getElementById('weather-display');
 
     fetch(url)
@@ -10,12 +10,12 @@ document.addEventListener('DOMContentLoaded', () => {
             return response.json();
         })
         .then(data => {
-            const location = data.location.name;
+            // const location = data.location.name;
             const tempC = data.current.temp_c;
-            const humidity = data.current.humidity;
-            const windKph = data.current.wind_kph;
-            const conditionText = data.current.condition.text;
-            const conditionIcon = data.current.condition.icon;
+            // const humidity = data.current.humidity;
+            // const windKph = data.current.wind_kph;
+            // const conditionText = data.current.condition.text;
+            // const conditionIcon = data.current.condition.icon;
 
             weatherDisplay.innerHTML = `
             <span class="text-gray-600 text-sm md:text-base">
