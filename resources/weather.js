328373364c00a77b73ead86fae34f0fb1aadada6
ICTConsultant <jugroopsinghjugpal@gmail.com>
2025-09-01 14:54:40 +0100

    https

--- resources/weather.js
+++ resources/weather.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const url = `https://api.weatherapi.com/v1/current.json?key=6a4e09f9f46e4a75a67171320251208&q=B76&aqi=no`;
+    const url = "https://api.weatherapi.com/v1/current.json?key=6a4e09f9f46e4a75a67171320251208&q=B76&aqi=no";
     const weatherDisplay = document.getElementById('weather-display');
 
     fetch(url)
@@ -17,14 +17,14 @@ document.addEventListener('DOMContentLoaded', () => {
             // const conditionText = data.current.condition.text;
             // const conditionIcon = data.current.condition.icon;
 
-            weatherDisplay.innerHTML = `
+            weatherDisplay.innerHTML = "
             <span class="text-gray-600 text-sm md:text-base">
                 ${tempC}°C
             </span>
-            `;
+            ";
         })
         .catch(error => {
-            weatherDisplay.innerHTML = `<p style="color: red;">Error fetching weather data: ${error.message}</p>`;
+            weatherDisplay.innerHTML = "<p style="color: red;">Error fetching weather data: ${error.message}</p>";
             console.error('Weather API: Error:', error);
         });
 });
